bd07031c0d19af5c54324bc856f04e72879c36f9
cozysaint <anjs112@naver.com>
2024-02-18 21:47:14 +0900

    awful, but work

--- ppt2text.py
+++ ppt2text.py
@@ -4,10 +4,10 @@ import tempfile
 import pytesseract
 from PIL import Image
 
-def extract_text_from_image(image_path):
+def extract_text_from_image(image_path, lang='kor'):
     # 이미지에서 텍스트 추출
     img = Image.open(image_path)
-    text = pytesseract.image_to_string(img)
+    text = pytesseract.image_to_string(img, lang=lang)
     return text
 
 def replace_images_with_text(pptx_file):
@@ -20,7 +20,7 @@ def replace_images_with_text(pptx_file):
                     img_path = tmp_file.name  # 임시 이미지 저장 경로
                     with open(img_path, "wb") as f:
                         f.write(shape.image.blob)  # 이미지 데이터를 파일로 저장
-                text = extract_text_from_image(img_path)
+                text = extract_text_from_image(img_path, lang='kor')
                 os.remove(img_path)  # 임시 이미지 파일 삭제
 
                 # 텍스트 상자로 변환
